b1bcb37a3811d991397d89ae2d57aedecd78f6a3
Joseph Henry <josephenry@protonmail.com>
2021-07-29 00:33:04 +0200

    docs: :memo: add docs for GridLocation

--- src/GridLocation.ts
+++ src/GridLocation.ts
@@ -1,3 +1,7 @@
+/**
+ * A GridLocation is a two dimensional vector like
+ * representing a location on a Grid
+ */
 export class GridLocation {
   row: number;
   col: number;
@@ -7,10 +11,20 @@ export class GridLocation {
     this.col = col;
   }
 
-  add(rowOffset: number, colOffset: number): GridLocation {
+  /**
+   * Add the given offset to the current location and return a new instance
+   * @param rowOffset row offset
+   * @param colOffset column offset
+   * @returns A new location
+   */
+  public add(rowOffset: number, colOffset: number): GridLocation {
     return new GridLocation(this.row + rowOffset, this.col + colOffset);
   }
 
+  /**
+   *
+   * @returns The string representation of a location
+   */
   public toString(): string {
     return `(${this.col}, ${this.row})`;
   }
